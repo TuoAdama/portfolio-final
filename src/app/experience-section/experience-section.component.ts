483704c0145cf7fa58385707efe2e2bdae81be33
TuoAdama <tuoadama17@gmail.com>
2024-09-11 22:07:32 +0200

    feat: experiences-section

---
+++ src/app/experience-section/experience-section.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input} from '@angular/core';
+import {ExperienceItemComponent} from "../experience-item/experience-item.component";
+
+@Component({
+  selector: 'app-experience-section',
+  standalone: true,
+  imports: [
+    ExperienceItemComponent
+  ],
+  templateUrl: './experience-section.component.html',
+  styleUrl: './experience-section.component.css'
+})
+export class ExperienceSectionComponent {
+  @Input({required: true}) name: string = "";
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 import {NavBarComponent} from "../nav-bar/nav-bar.component";
 import {ExperienceItemComponent} from "../experience-item/experience-item.component";
+import {ExperienceSectionComponent} from "../experience-section/experience-section.component";
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,8 @@ import {ExperienceItemComponent} from "../experience-item/experience-item.compon
   imports: [
     NgOptimizedImage,
     NavBarComponent,
-    ExperienceItemComponent
+    ExperienceItemComponent,
+    ExperienceSectionComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'

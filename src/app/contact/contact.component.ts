6fc7ae7d592551bb5483d5d433d7d1dd89b1b132
TuoAdama <tuoadama17@gmail.com>
2024-09-12 00:18:04 +0200

    feat: contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {Contact} from "../../models/Contact";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+   form: FormGroup = new FormGroup({
+     email: new FormControl("", [Validators.required, Validators.email]),
+     title: new FormControl("", [Validators.required, Validators.minLength(3)]),
+     message: new FormControl("", [Validators.required, Validators.minLength(3)]),
+   })
 
+  onSubmit(){
+    alert(this.form.value.email);
+  }
 }

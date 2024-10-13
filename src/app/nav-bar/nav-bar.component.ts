e178db86ce201d5f1c92fd2d4ed7c7dde4a1e048
TuoAdama <tuoadama17@gmail.com>
2024-10-13 11:29:23 +0200

    feat: added nav-bar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -11,5 +11,9 @@ import {RouterLink} from "@angular/router";
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-
+   readonly networks = {
+     github: "https://github.com/TuoAdama",
+     twitter: "https://x.com/demci_tuo",
+     linkedin: "https://www.linkedin.com/in/adama-tuo-6a8132a1",
+  }
 }

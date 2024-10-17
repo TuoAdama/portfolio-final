0e1e3520196b1bb8c0d60318a673d2e07587b6f4
TuoAdama <tuoadama17@gmail.com>
2024-10-17 21:54:33 +0200

    feat: change workspace

--- src/app/silder/silder.component.ts
+++ src/app/silder/silder.component.ts
@@ -1,25 +1,41 @@
-import {Component, OnInit} from '@angular/core';
-import Swiper from "swiper";
+import {
+  AfterViewInit,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  ElementRef,
+  OnInit,
+  QueryList,
+  OnDestroy,
+  ViewChild, ViewChildren
+} from '@angular/core';
+import {interval} from "rxjs";
 
 @Component({
   selector: 'app-silder',
   standalone: true,
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './silder.component.html',
   styleUrl: './silder.component.css'
 })
-export class SilderComponent implements OnInit{
-  ngOnInit(): void {
-    let swiper = new Swiper(".default-carousel", {
-      loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+export class SilderComponent implements OnInit, AfterViewInit, OnDestroy{
+  @ViewChildren('slideItem') slidesItems!: QueryList<ElementRef>;
+  index: number = 0;
+  indexActive = 1;
+  intervalId: any;
+
+  ngAfterViewInit(): void {
+    this.intervalId = setInterval(() => {
+      console.log(this.index++);
+    }, 2000);
   }
+
+  ngOnInit(): void {
+    console.log("Coucou la planete")
+  }
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+
+
 }

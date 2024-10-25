dc407314a9556a77a3bac038dd515630f1cbb137
TuoAdama <tuoadama17@gmail.com>
2024-10-25 19:54:05 +0200

    feat: theme integration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+
+    },
+    fontFamily: {
+      sans: ['Geist', 'Inter', ...defaultTheme.fontFamily.sans],
+      mono : ['GeistMono', 'fira-code', ...defaultTheme.fontFamily.mono],
+    },
+    keyframes: {
+      loop: {
+        to: {
+          "offset-distance": "100%",
+        },
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),

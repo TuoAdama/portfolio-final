af88e275166a10cacb01b557609baeeac40e3b89
TuoAdama <tuoadama17@gmail.com>
2024-10-26 18:50:42 +0200

    feat: added link for componements

--- src/app/logo/logo.component.ts
+++ src/app/logo/logo.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-logo',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './logo.component.html',
   styleUrl: './logo.component.css'
 })

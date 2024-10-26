4e50de217e77849649d291d49c0acdd93c80a759
TuoAdama <tuoadama17@gmail.com>
2024-10-26 16:52:34 +0200

    feat: added logo

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {LogoComponent} from "../logo/logo.component";
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    LogoComponent
   ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
